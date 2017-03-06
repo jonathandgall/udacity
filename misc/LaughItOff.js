ebf7c87027440edf0eb776fb23972a5595a80714
Jonathan Gall <odajay@gmail.com>
2017-03-06 13:44:30 +0100

    Adding new JavaScript exercises

---
+++ misc/LaughItOff.js
@@ -0,0 +1,11 @@
+/*
+ * Programming Quiz: Laugh it Off 1 (5-1)
+ */
+
+// your code goes here
+function laugh() {
+	var foo = "hahahahahahahahahaha!";
+	return foo
+}
+
+console.log(/* call the laugh function */laugh());

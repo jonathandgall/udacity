856830de57a1de37ebbee1ff9da6abb94062ed37
Jonathan Gall <odajay@gmail.com>
2017-04-19 08:54:43 +0200

    Adding Ajax request with jQuery $.getJSON method for NYTimes articles via their API

--- js/script.js
+++ js/script.js
@@ -5,19 +5,42 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
-    var $streetStr = $('#street').val();
-    var $cityStr = $("#city").val();
-    // var $concat = $street+","+$city;
+
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
 
-    // YOUR CODE GOES HERE!
-    $body.append('<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=1280x900&location='+$streetStr+','+$cityStr+'">');
+    //GoogleStreetView
+    var streetStr = $('#street').val();
+    var cityStr = $("#city").val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+    //NYTimes Ajax Request
+
+    var items = [];
+    var nyTimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + address + '&page=1&sort=newest&api-key=b6937703c0834ab6a5c30e3b668b104a'
+
+    $.getJSON(nyTimesUrl, function(data) {
+
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        $.each(data.response.docs, function(key, val) {
+            items.push('<li class="article"> <a href="' + data.response.docs[key].web_url + '">' + data.response.docs[key].headline.main + '</a>' + '<p>' + data.response.docs[key].snippet + '</p>' + '</li>');
+        })
+
+        for (var i = 0; i <= items.length - 1; i++) {
+            $nytElem.append(items[i]);
+        }
+    });
+
     return false;
 };
+
 
 $('#form-container').submit(loadData);

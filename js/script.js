b0181cd98dd4e58fcc48d0dc8d31b4798017ea55
Jonathan Gall <odajay@gmail.com>
2017-05-03 20:18:58 +0200

    Reengineering of code to give the possibility to hide/show image when the name is clicked in a menu

--- js/script.js
+++ js/script.js
@@ -5,35 +5,63 @@ function loadData() {
     var $catname = $('.catname');
     var $numberclicks = $('.numberclicks');
     var $container = $('#container');
-
-    var nb0 = 0;
-    var nb1 = 0;
+    var $catlist = $('#catlist');
+    var $catpic = $('#catpic');
 
     var cats = [{
         "name": "Kiaousse",
-        "url": "http://thecatapi.com/api/images/get?format=src&type=jpg",
-        "clk": 0
+        "url": "./img/cat1.jpeg",
+        "clk": 0,
+        "hided": 1
     }, {
         "name": "Miagel",
-        "url": "http://lorempixel.com/400/200/cats",
-        "clk": 0
+        "url": "./img/cat2.jpeg",
+        "clk": 0,
+        "hided": 1
+    }, {
+        "name": "Karambrousse",
+        "url": "./img/cat3.jpeg",
+        "clk": 0,
+        "hided": 1
+    }, {
+        "name": "Miaousse",
+        "url": "./img/cat4.jpeg",
+        "clk": 0,
+        "hided": 1
+    }, {
+        "name": "Minaude",
+        "url": "./img/cat5.jpeg",
+        "clk": 0,
+        "hided": 1
     }];
 
 
     for (var i = 0; i <= cats.length - 1; i++) {
-        $container.append('<div class="catname">' + cats[i].name + '</div>' + '<img  width="400px" class="catimg" id="img' + i + '" src="' + cats[i].url + '"><p>Number of clicks: <span id="nbclicks' + i + '">' + cats[i].clk + '</span></p>');
+        $catlist.append('<div class="catMenu catList' + i + '">' + cats[i].name + '</div>');
+        $catpic.append('<div class="catname hide cat' + i + '">' + cats[i].name + '</div>' + '<img  width="400px" class="catimg hide" id="img' + i + '" src="' + cats[i].url + '"><p class="hide cat' + i + '">Number of clicks: <span id="nbclicks' + i + '">' + cats[i].clk + '</span></p>');
     };
 
     function createCallback(i) {
         return function() {
             cats[i].clk++;
-            $('#nbclicks'+i).text(cats[i].clk);
+            $('#nbclicks' + i).text(cats[i].clk);
         }
-    }
+    };
+
+    for (var i = 0; i <= cats.length; i++) {
+        $('.catList' + i).click(hide(i));
+    };
 
     for (var i = 0; i <= cats.length; i++) {
         $('#img' + i).click(createCallback(i));
-        };
+    };
+
+    function hide(i) {
+        return function() {
+            $('#img' + i).toggleClass("hide");
+            $('.cat' + i).toggleClass("hide");
+        }
+    }
 
     return false;
 };

e25286cfd11159211341919e2584be4c2a380b85
Jonathan Gall <odajay@gmail.com>
2017-05-04 16:02:32 +0200

    Adding the date in the 'Octopus' so that it is rendered on the output

--- js/retain.js
+++ js/retain.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function() {
 
     var model = {
         init: function() {
@@ -20,7 +20,7 @@ $(function(){
     var octopus = {
         addNewNote: function(noteStr) {
             model.add({
-                content: noteStr
+                content: noteStr +'<p class="note-date">' + formatDate(new Date()) + '</p>'
             });
             view.render();
         },
@@ -35,27 +35,42 @@ $(function(){
         }
     };
 
+    function formatDate(date) {
+        var monthNames = [
+            "January", "February", "March",
+            "April", "May", "June", "July",
+            "August", "September", "October",
+            "November", "December"
+        ];
+
+        var day = date.getDate();
+        var monthIndex = date.getMonth();
+        var year = date.getFullYear();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+
+        return day + ' ' + monthNames[monthIndex] + ' ' + year + ' ' + hours + 'h' + minutes + 'mn' + seconds + 's';
+    }
 
     var view = {
         init: function() {
             this.noteList = $('#notes');
             var newNoteForm = $('#new-note-form');
             var newNoteContent = $('#new-note-content');
-            newNoteForm.submit(function(e){
+            newNoteForm.submit(function(e) {
                 octopus.addNewNote(newNoteContent.val());
                 newNoteContent.val('');
                 e.preventDefault();
             });
             view.render();
         },
-        render: function(){
+        render: function() {
             var htmlStr = '';
-            octopus.getNotes().forEach(function(note){
-                htmlStr += '<li class="note">'+
-                        note.content +
-                    '</li>';
+            octopus.getNotes().forEach(function(note) {
+                htmlStr += '<li class="note">' + note.content + '</li>'; //+ '<ul>' + formatDate(new Date()) + '</ul>' + formatDate(new Date());
             });
-            this.noteList.html( htmlStr );
+            this.noteList.html(htmlStr);
         }
     };
 

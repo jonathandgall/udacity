33c4f6688aa527eed6a140c54188223aff7eb46d
Jonathan Gall <odajay@gmail.com>
2017-05-20 23:26:40 +0200

    Implementing levels

--- fend/ud989-cat-clicker-ko/js/app.js
+++ fend/ud989-cat-clicker-ko/js/app.js
@@ -1,12 +1,24 @@
 var ViewModel = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
+    this.level = ko.computed(function() {
+        if (this.clickCount() < 10) {
+            return "NewBorn"
+        } else if (this.clickCount() < 20) {
+            return "Infant"
+        } else {return "Teen"}
+    }, this)
 
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1)
-	}
+
+
+
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1)
+    }
+
+
 }
 
 ko.applyBindings(new ViewModel())

3e9951e786b0fe303973506c5ce13dcdbab09659
Jonathan Gall <odajay@gmail.com>
2017-05-20 23:58:06 +0200

    Implementing Control Flow in order to show nickname using foreach binding with ko.observablearray

--- fend/ud989-cat-clicker-ko/js/app.js
+++ fend/ud989-cat-clicker-ko/js/app.js
@@ -3,6 +3,8 @@ var ViewModel = function() {
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
+    this.nicknames = ko.observableArray([{nick:"Tabtab"},{nick:"T-Bone"},{nick:"Mr. T"}, {nick: "Tabitha Tab Tabby Cat"
+}])
     this.level = ko.computed(function() {
         if (this.clickCount() < 10) {
             return "NewBorn"

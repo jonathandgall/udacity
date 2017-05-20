89eb37cdd6a0bfb1aa6bf6b15bc4ca3f970a811f
Jonathan Gall <odajay@gmail.com>
2017-05-20 22:12:07 +0200

    Adding ViewModel method

--- fend/ud989-cat-clicker-ko/js/app.js
+++ fend/ud989-cat-clicker-ko/js/app.js
@@ -4,6 +4,9 @@ var ViewModel = function() {
 	this.imgSrc = ko.observable('img/1413379559_412a540d29_z.jpg');
 	this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
 
+	this.incrementCounter = function() {
+		this.clickCount(this.clickCount() + 1)
+	};
 }
 
 ko.applyBindings(new ViewModel())

05ef1562fe797d1f45bc33c4ba0c96f9f973153f
Jonathan Gall <odajay@gmail.com>
2017-05-21 00:11:20 +0200

    Separating out the model by creating a Cat function dedicated to it

--- fend/ud989-cat-clicker-ko/js/app.js
+++ fend/ud989-cat-clicker-ko/js/app.js
@@ -1,26 +1,33 @@
-var ViewModel = function() {
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
-    this.nicknames = ko.observableArray([{nick:"Tabtab"},{nick:"T-Bone"},{nick:"Mr. T"}, {nick: "Tabitha Tab Tabby Cat"
-}])
+    this.nicknames = ko.observableArray([
+        { nick: "Tabtab" },
+        { nick: "T-Bone" },
+        { nick: "Mr. T" },
+        { nick: "Tabitha Tab Tabby Cat" }
+    ]);
     this.level = ko.computed(function() {
         if (this.clickCount() < 10) {
             return "NewBorn"
         } else if (this.clickCount() < 20) {
             return "Infant"
-        } else {return "Teen"}
+        } else {
+            return "Teen"
+        }
     }, this)
+};
 
+var ViewModel = function() {
 
-
+    this.currentCat = ko.observable(new Cat());
 
     this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1)
-    }
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1)
+    };
 
-
-}
+};
 
 ko.applyBindings(new ViewModel())

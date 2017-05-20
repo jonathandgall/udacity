652e64d86e0185c3ab9c9d89a446d130abbe627d
Jonathan Gall <odajay@gmail.com>
2017-05-20 22:36:04 +0200

    Include databindings in html

--- fend/ud989-cat-clicker-ko/js/app.js
+++ fend/ud989-cat-clicker-ko/js/app.js
@@ -1,12 +1,12 @@
 var ViewModel = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/1413379559_412a540d29_z.jpg');
+	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1)
-	};
+	}
 }
 
 ko.applyBindings(new ViewModel())

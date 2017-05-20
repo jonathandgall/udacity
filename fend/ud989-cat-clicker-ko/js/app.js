b25f7f3359124839d767c45f51823eed66fdf26e
Jonathan Gall <odajay@gmail.com>
2017-05-20 22:07:54 +0200

    Implementing Model inside the ViewModel

--- fend/ud989-cat-clicker-ko/js/app.js
+++ fend/ud989-cat-clicker-ko/js/app.js
@@ -1,4 +1,8 @@
 var ViewModel = function() {
+	this.clickCount = ko.observable(0);
+	this.name = ko.observable('Tabby');
+	this.imgSrc = ko.observable('img/1413379559_412a540d29_z.jpg');
+	this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
 
 }
 

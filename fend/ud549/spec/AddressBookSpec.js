d93d6a911e3ac28759aee1f1d87dcebbdcc3b58c
Jonathan Gall <odajay@gmail.com>
2017-06-04 12:12:58 +0200

    Writing AddressBookSpecs.js

---
+++ fend/ud549/spec/AddressBookSpec.js
@@ -0,0 +1,11 @@
+decribe('Address Book', function(){
+	it('should be able to find a contact',function() {
+	var addressBook = new AddressBook(),
+		thisContact = new Contact();
+
+	addressBook.addContact(thisContact);
+
+	expect(addressBook.getContact(0)).toBe(thisContact);
+
+	});
+});

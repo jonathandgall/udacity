92092a6d7946206ac7e3fb4d858f7bd6a11d68bb
Jonathan Gall <odajay@gmail.com>
2017-06-04 16:45:41 +0200

    Implementation of contact deletion for test with splice method

--- fend/ud549/src/AddressBook.js
+++ fend/ud549/src/AddressBook.js
@@ -1,11 +1,15 @@
 function AddressBook() {
-	this.contacts = [];
+    this.contacts = [];
 };
 
 AddressBook.prototype.addContact = function(contact) {
-	this.contacts.push(contact)
+    this.contacts.push(contact)
 };
 
 AddressBook.prototype.getContact = function(index) {
-	return this.contacts[index];
+    return this.contacts[index];
+};
+
+AddressBook.prototype.deleteContact = function(index) {
+    this.contacts.splice(index, 1);
 };
